bb68198e5451f763c220a5cd4a6f7eee2b0cd8fb
asdfgshfgf <144590364+Vasyandragit@users.noreply.github.com>
2026-04-17 17:51:48 +0300

    Update news_parser.py

--- news_parser.py
+++ news_parser.py
@@ -3,28 +3,32 @@ import asyncio
 
 class NewsParser:
 
-    async def get_news(self, keyword):
+    async def get_news(self, domain):
         BASE_URL = "https://newsapi.org/v2/everything" 
         apiKey = "785049a5787047ed81eed7eb03287512"
-        results = 100
+        results = 1000
 
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"{BASE_URL}?q={keyword}&apikey={apiKey}") as response:
+            async with session.get(f"{BASE_URL}?q={domain}&apikey={apiKey}&language=ru") as response:
                 data = await response.json()
+        
 
         news_list = []
-     
-        print(data)
         for article in data["articles"][:results]:
             title = article["title"]
             desc = article["description"]
             url = article["url"]
 
-            news_list.append({
-                "title" : title,
-                "desc" : desc,
-                "url" : url
-                }) 
+            for keyword in ["блок", "упал", "замедле", "огранич", "недоступ", "сбой"]:
+                
+                if keyword in title.lower():
+                    news_list.append(
+                        f"📰 {title}\n"
+                        f"{desc}\n"
+                        f"{url}\n"
+                        )
+
+                    break
     
         return news_list
 
